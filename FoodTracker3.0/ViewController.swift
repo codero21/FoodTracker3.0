6e6aac7570bde6e961f1b86cba6d70d00867838a
Rollin Francois <Rollin@rollins-mbp.attlocal.net>
2016-10-02 14:30:15 -0400

    Adjust the text filed width in the meal scene

--- FoodTracker3.0/ViewController.swift
+++ FoodTracker3.0/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
