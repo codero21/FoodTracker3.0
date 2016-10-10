f62a793278128fc87aa811759a3383e864a18568
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-09 23:19:30 -0400

    Implement the imagePickerControllerDidCancel(_:) method / Add image to simulator /  Add NSPhotoLibraryUsageDescription key (String) to the info.plist file

--- FoodTracker3.0/ViewController.swift
+++ FoodTracker3.0/ViewController.swift
@@ -42,6 +42,22 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         mealNameLabel.text = textField.text
     }
 
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     
     // MARK: Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
